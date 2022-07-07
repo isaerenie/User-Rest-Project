9b9fb3e3ce8d8b6ca95693f802cdd35f41a6cbfd
isa Eren <105389265+Eren100@users.noreply.github.com>
2022-07-07 10:07:08 +0300

    Entity

--- src/main/java/com/works/entities/User.java
+++ src/main/java/com/works/entities/User.java
@@ -3,6 +3,7 @@ package com.works.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 
@@ -14,8 +15,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int uid;
-
+    @Length(min = 2, max = 30, message = "Karakter uzunluğu minimum 2 maksimum 30 olmalıdır.")
     private String name;
+    @Length(min = 2, max = 30, message = "Karakter uzunluğu minimum 2 maksimum 30 olmalıdır.")
     private String surname;
 
     @Column(unique = true, length = 150)
